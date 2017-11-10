e29aae270b1b8af6ca94912d1ebcee121a83633e
Anastasia Kupriyanova <anastasy.kuprianova@gmail.com>
2017-11-10 16:28:26 -0500

    Added 'express-session', login form, and authentication to user model

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Dependencies
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -7,7 +8,15 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var sassMiddleware = require('node-sass-middleware');
+var session = require('express-session');
 var app = express();
+
+// Sessions
+app.use(session({
+  secret: 'htmlcore',
+  resave: true,
+  saveUninitialized: false
+}));
 
 // MongoDB
 var mongoose = require('mongoose');
@@ -22,6 +31,7 @@ app.set('view engine', 'ejs');
 // Favicon
 //app.use(favicon(path.join(__dirname, 'assets', 'favicon.ico')));
 
+// Render
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -41,22 +51,24 @@ app.use(express.static(path.join(__dirname, 'assets')));
 var routes = require('./routes/index');
 app.use('/', routes);
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to Error Handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   return next(err);
 });
 
-// error handler
+// Error Handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+
+  // Set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);
   return res.render('error');
+
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,61 @@ let express = require('express');
 let router = express.Router();
 let User = require('../models/user');
 
-// GET
+// GET /profile
+router.get('/profile', function (req, res, next) {
+  if (! req.session.userId ) {
+    var err = new Error('You are not authorized to view this page.')
+    err.status = 403;
+    return next(err);
+  }
+  User.findById(req.session.userId).exec(function( error, user ) {
+    if (error) {
+      return next(error);
+    } else {
+      return res.render('profile', {
+        title: 'Profile: ' + user.alias,
+        alias: user.alias
+      });
+    }
+  });
+});
+
+// GET /login
+router.get('/login', function (req, res, next) {
+  return res.render('login', {
+    title: 'User Login'
+  });
+});
+
+// POST /login
+router.post('/login', function (req, res, next) {
+  if (req.body.email && req.body.password) {
+    // Authenticate
+    User.authenticate(req.body.email, req.body.password, function (error, user) {
+      if (error || !user) {
+        var err = new Error('Wrong email or password.')
+        err.status = 401;
+        return next(err);
+      } else {
+        req.session.userId = user._id;
+        return res.redirect('/profile');
+      }
+    });
+  } else {
+    let err = new Error('Email and password are required.');
+    err.status = 401;
+    return next(err);
+  }
+});
+
+// GET /register
 router.get('/register', function (req, res, next) {
   return res.render('register', {
     title: 'User Registration'
   });
 });
 
-// POST
+// POST /register
 router.post('/register', function (req, res, next) {
 
     if (req.body.email &&
@@ -35,6 +82,7 @@ router.post('/register', function (req, res, next) {
         if (error) {
           return next(error);
         } else {
+          req.session.userId = user._id;
           return res.redirect('/profile');
         }
       });
