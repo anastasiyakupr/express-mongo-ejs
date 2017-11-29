7ff1806acf881b980300385fa7ca02dd18fb707e
Anastasia Kupriyanova <anastasy.kuprianova@gmail.com>
2017-11-29 10:16:41 -0500

    Removed unwanted custom middleware

--- middleware/index.js
+++ middleware/index.js
@@ -1,39 +1,5 @@
 'use strict';
 
-// let User = require('../models/user');
-// function lastUpdated(req, res, next) {
-// 	let userData = {
-// 		$set: {updatedOn: Date.now()}
-// 	};
-// 	User.update(userData, function (error) {
-// 		if (error) {
-// 			return next(error);
-// 		} else {
-// 			return next();
-// 		}
-// 	});
-// }
-
-let User = require('../models/user');
-function lastUpdated(req, res, next) {
-	User.findById(req.session.userId).exec(function( error, user ) {
-    if (error) {
-      return next(error);
-    } else {
-			// let userData = {
-			// 	updatedOn: Date.now()
-			// };
-			// User.update(userData, function (error) {
-			// 	if (error) {
-			// 		return next(error);
-			// 	} else {
-			// 		return next();
-			// 	}
-			// });
-		}
-	});
-}
-
 function loggedOut(req, res, next) {
 	if (req.session && req.session.userId) {
 		return res.redirect('/profile');
@@ -51,6 +17,5 @@ function requiresLogin(req, res, next) {
 	}
 }
 
-// module.exports.lastUpdated = lastUpdated;
 module.exports.loggedOut = loggedOut;
 module.exports.requiresLogin = requiresLogin;
