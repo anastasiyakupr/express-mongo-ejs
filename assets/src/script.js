853690035e806059ab50e964f8a9542e8e5d1db7
Anastasia Kupriyanova <anastasy.kuprianova@gmail.com>
2018-03-18 01:42:56 -0400

    Added hidden 'Developer Tools' component (displayed via three clicks on developer's email in footer)

--- assets/src/script.js
+++ assets/src/script.js
@@ -1,1 +1,58 @@
-console.log('Hello, World\!');
+"use strict";
+
+// Initial Notification
+console.log("The file 'script.min.js' has loaded...");
+
+// Developer Tools
+const devTools = document.createElement('DIV');
+devTools.setAttribute('id', 'devTools');
+
+const dev = document.getElementById('developer');
+
+let toggle = 0;
+
+
+let viewportWidth = window.innerWidth,
+		viewportHeight = window.innerHeight;
+
+let viewportSize = `
+	<div class="viewportSize">
+		<div>Width: ${viewportWidth}</div>
+		<div>Height: ${viewportHeight}</div>
+	</div>
+`;
+
+devTools.insertAdjacentHTML('beforeend', viewportSize);
+
+window.addEventListener('resize', () => {
+	viewportWidth = window.innerWidth;
+	viewportHeight = window.innerHeight;
+	viewportSize = `
+			<div class="viewportSize">
+				<div>Width: ${viewportWidth}</div>
+				<div>Height: ${viewportHeight}</div>
+			</div>
+		`;
+
+	while (devTools.firstChild) {
+		devTools.removeChild(devTools.firstChild);
+	}
+
+	devTools.insertAdjacentHTML('beforeend', viewportSize);
+
+}, false);
+
+dev.addEventListener('click', () => {
+	if (toggle < 3) {
+		toggle += 1;
+		console.log(toggle);
+		if (toggle === 3) {
+			devTools.insertAdjacentHTML('beforeend', viewportSize);
+			dev.appendChild(devTools);
+		}
+	} 
+});
+
+
+
+
