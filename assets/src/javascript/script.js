03e70507e7b6d359e155da7d1dba30dd9ecc094b
Anastasia Kupriyanova <anastasy.kuprianova@gmail.com>
2018-03-25 23:04:45 -0400

    Worked on styling 'Admin  : Users', fixed sizing issue with top menu

--- assets/src/javascript/script.js
+++ assets/src/javascript/script.js
@@ -4,10 +4,25 @@
 console.log("The file 'script.min.js' has loaded...");
 
 // Developer Tools
+const dev = document.getElementById('developer');
+
+// Dev Tools : Easter Egg Launcher
+dev.addEventListener('click', () => {
+	if (toggle < 3) {
+		toggle += 1;
+		if (toggle === 3) {
+			devTools.insertAdjacentHTML('beforeend', viewportSize);
+			dev.appendChild(devTools);
+		}
+	} else {
+		toggle = 0;
+		dev.removeChild(devTools);
+	}
+	console.log(toggle);
+});
+
 const devTools = document.createElement('div');
 devTools.setAttribute('id', 'devTools');
-
-const dev = document.getElementById('developer');
 
 let toggle = 0;
 
@@ -41,17 +56,41 @@ window.addEventListener('resize', () => {
 
 }, false);
 
-dev.addEventListener('click', () => {
-	if (toggle < 3) {
-		toggle += 1;
-		console.log(toggle);
-		if (toggle === 3) {
-			devTools.insertAdjacentHTML('beforeend', viewportSize);
-			dev.appendChild(devTools);
-		}
-	} 
+// USER - Edit
+const userEdit = document.querySelector("form button.userEdit");
+userEdit.addEventListener('click', (e) => {
+	e.preventDefault();
 });
 
+// USER - Delete
+const userDelete = document.querySelector("form button.userDelete");
+userDelete.addEventListener('click', (e) => {
+	
+	e.preventDefault();
+	
+	console.log("Verify");
+	
+	const modal = document.createElement('div');
+	modal.setAttribute('id','modal');
 
+	const verify = `
+		<div class="modal-card">
+			<h3>Are you sure?</h3>
+			<button value="true">Yes</button>
+			<button value="false">No</button>
+		</div>
+	`;
+	modal.insertAdjacentHTML('beforeend', verify);
+	document.body.appendChild(modal);
 
+	document.addEventListener('keyup', (e) => {
+    e = e || window.event;
+    if (e.keyCode == 27) {
+			document.body.removeChild(modal);
+		}
+	});
+
+	let confirm = false;
+	console.log("Delete User");
+});
 
